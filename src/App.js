1d12a5be8b373b39cf2aed89888b339824b134fa
RParmasar <rajeev@parmasar.com>
2021-01-15 03:22:21 -0500

    Added Jumbotron and Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
+import {Jumbotron, Container, Navbar, Button} from 'react-bootstrap';
+
+import jumbobg from "./assets/bglandingpage.jpg";
+import logo from "./assets/SVILogoUpdatedColours.svg";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
-      
-      
+     <Navbar id="svi-nav" sticky="top">
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            width="50"
+            height="50"
+            className="d-inline-block align-top"
+            alt="SVI logo"
+          />
+        </Navbar.Brand>
+     </Navbar>
+     <Jumbotron>
+        <Container>
+          <h1>We help our investors secure their financial futures through real estate.</h1>
+          <p>
+            <Button variant="primary">Learn more</Button>
+          </p>
+        </Container>
+    </Jumbotron>
     </div>
   );
 }
